ea827d36866547215b079a5f59031afebafff594
Bestami <bestamibakir@gmail.com>
2026-04-23 17:18:44 +0300

    fix: update password generation method in UserDataBuilder to use faker.credentials()

--- src/test/java/com/bestamibakir/data/UserDataBuilder.java
+++ src/test/java/com/bestamibakir/data/UserDataBuilder.java
@@ -7,7 +7,7 @@ public class UserDataBuilder {
     public static User generateRandomUser() {
         Faker faker = new Faker();
         // JuiceShop şifre politikasına uyması için kompleks bir şifre üretiyoruz
-        String password = faker.internet().password(8, 15, true, true, true) + "1aA!";
+        String password = faker.credentials().password(8, 15, true, true, true) + "1aA!";
         
         return User.builder()
                 .email(faker.internet().emailAddress())
